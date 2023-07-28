5f20e1cf5d3a4c2b69b72afe59ac15dfd9ce4137
wojwoj8 <wojwoj8@gmail.com>
2023-07-28 17:15:53 +0200

    simple profile form UI

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Icon from '@mdi/react';
+import { mdiPencil } from '@mdi/js';
 
 interface ProfileData{
     first_name: string;
@@ -9,19 +11,66 @@ interface ProfileData{
 }
 
 const Profile: React.FC = () =>{
-    const [profile, setProfile] = useState<ProfileData | null>(null)
+    const [profile, setProfile] = useState<ProfileData | null>(null);
+    const [editing, setEditing] = useState(false);
+
+    const editProfile = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>{
+        // e.preventDefault()
+        setEditing(!editing);
+    }
+
     
+
     return(
         <div className="container-sm">
-            <div className="container-fluid text-center">
-                <span>First Name Last Name</span>
-                <p>Date of birth, Country, city</p>
+            <div className="container-fluid">
+                <div className='text-center'>
+                    <button className='btn btn-secondary' onClick={e => editProfile(e)}>
+                        <Icon path={mdiPencil} size={1} />
+                    </button>
+                    <span>First Name Last Name</span>
+                    <p>Date of birth, Country, city</p>
+                </div>
                 
+                {editing &&  
+                <div className="container">
+                    <form>
+                        <div className='row'>
+                            <div className='mb-3 col-4'>
+                                <label htmlFor='first_name' className="form-label">First name:</label>
+                                <input type='text' name='first_name' className="form-control" placeholder='John'></input>
+                            </div>
+                            <div className='mb-3 col-4'>
+                                <label htmlFor='last_name' className="form-label">Last name:</label>
+                                <input type='text' name='last_name' className="form-control" placeholder='Smith'></input>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='mb-3 col-4'>
+                                <label htmlFor='date_of_birth' className="form-label">Date of Birth:</label>
+                                <input type='date' name='date_of_birth' className="form-control" ></input>
+                            </div>
+                            <div className='mb-3 col-4'>
+                                <label htmlFor='country' className="form-label">Country:</label>
+                                <input type='text' name='country' className="form-control" placeholder='Poland'></input>
+                            </div>
+                            <div className='mb-3 col-4'>
+                                <label htmlFor='city' className="form-label">City:</label>
+                                <input type='text' name='city' className="form-control" placeholder='Radom'></input>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+                }
             </div>
+                
+            {editing && 
             <div className='text-center'>
                 <button className='btn btn-secondary'>Cancel</button>
                 <button className='btn btn-primary'>Save</button>
             </div>
+            }
         </div>
     )
 }

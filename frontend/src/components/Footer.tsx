b2a3de50a28aae1f71b319d85d298f31e38953c0
wojwoj8 <wojwoj8@gmail.com>
2023-08-22 22:56:14 +0200

    better footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -18,39 +18,15 @@ const Footer: React.FC = () =>{
                 
         //         </div>
         // </footer>
-        <footer className="navbar d-flex px-0 px-sm-3 flex-wrap justify-content-between align-items-center py-2 mt-4 border-top bg-body-tertiary " data-bs-theme="dark">
-            <p className="col-sm-4 mb-0 ps-2 pe-sm-0 pe-2 text-white">AppliGate</p>
+        <footer className="navbar border-top bg-body-tertiary justify-content-center " data-bs-theme="dark">
+  <div className="d-flex justify-content-between align-items-center px-4 py-2">
+    <Link to="https://github.com/wojwoj8" className="text-decoration-none d-flex align-items-center">
+      <span className="me-2">Created by wojwoj8</span>
+      <Icon path={mdiGithub} size={1} className="text-white" />
+    </Link>
 
-             <Link to='https://github.com/wojwoj8' className='col-sm-4 d-flex align-items-center justify-content-center mb-0 mx-4 mx-sm-0 mb-sm-3 mb-md-0 me-md-auto  text-decoration-none'>
-                Created by wojwoj8 <Icon className='text-white' path={mdiGithub} size={1} />
-                </Link>
-
-
-                <ul className="nav navbar-nav col-sm-4 d-flex flex-row justify-content-end">
-                    {user ? (
-                        <>
-                        <li className="nav-item">
-                            <a href="/" className="nav-link px-2">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/profile" className="nav-link px-2">Profile</a>
-                        </li>
-                        <li className="nav-item" onClick={logoutUser}>
-                            <a href="/" className="nav-link px-2">Log Out</a>
-                        </li>
-                        </>
-                    ) : (
-                        <>
-                        <li className="nav-item">
-                            <a href="/login" className="nav-link px-2">Log In</a>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/register" className="nav-link px-2">Sign Up</a>
-                        </li>
-                        </>
-                    )}
-                    </ul>
-        </footer>
+  </div>
+</footer>
     )
     
 }

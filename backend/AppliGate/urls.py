4ed0312099913c347de7114f3e19aef7e14aa7cf
wojwoj8 <wojwoj8@gmail.com>
2023-10-15 18:09:38 +0200

    working backend test

--- backend/AppliGate/urls.py
+++ backend/AppliGate/urls.py
@@ -31,7 +31,7 @@ urlpatterns = [
     path("", include("user_app.urls")),
     # Authentication
     path("api/", include("user_app.api.urls")),
-]
+]   
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

247ddd2ca897c2b6a0502fdbde8020345652ef1b
wojwoj8 <wojwoj8@gmail.com>
2023-11-03 18:47:26 +0100

    better skills

--- frontend/src/components/JobOffers/JobOfferComponents/JobOfferSkill.tsx
+++ frontend/src/components/JobOffers/JobOfferComponents/JobOfferSkill.tsx
@@ -143,65 +143,73 @@ interface JobOfferSkillProps {
                         </form>
                     </div>
                     <div className=''>
-                        <p className="mt-2">Required</p>
-                        <div className='row flex mb-3 row-gap-2 column-gap-3 container'>
-                            {jobOfferSkill
-                                .filter(jobOfferSkill => jobOfferSkill.skill_type === 'required')
-                                .map((jobOfferSkill, index) => (
-                                    <div key={index} className='col-auto p-0'>
-                                        <div className='profile-skill border border-1 text-white bg-black rounded-2 p-1 px-2 d-flex align-items-center'>
-                                            <p className='mb-0 '>{jobOfferSkill?.skill || ''}
-                                                <svg
-                                                    height="0.75rem"
-                                                    width="0.75rem"
-                                                    version="1.1"
-                                                    id="Capa_1"
-                                                    viewBox="0 0 460.775 460.775"
-                                                    className='mb-1'
-                                                    onClick={() => deleteSkill(jobOfferSkill.id)}
-                                                >
-                                                    <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
-                                                    c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
-                                                    c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
-                                                    c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
-                                                    l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
-                                                    c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
-                                                </svg>
-                                            </p>
+                    {jobOfferSkill.some(skill => skill.skill_type === 'required') && (
+                        <>
+                            <p className="mt-2">Required</p>
+                            <div className='row flex mb-3 row-gap-2 column-gap-3 container'>
+                                {jobOfferSkill
+                                    .filter(jobOfferSkill => jobOfferSkill.skill_type === 'required')
+                                    .map((jobOfferSkill, index) => (
+                                        <div key={index} className='col-auto p-0'>
+                                            <div className='profile-skill border border-1 text-white bg-black rounded-2 p-1 px-2 d-flex align-items-center'>
+                                                <p className='mb-0 '>{jobOfferSkill?.skill || ''}
+                                                    <svg
+                                                        height="0.75rem"
+                                                        width="0.75rem"
+                                                        version="1.1"
+                                                        id="Capa_1"
+                                                        viewBox="0 0 460.775 460.775"
+                                                        className='mb-1'
+                                                        onClick={() => deleteSkill(jobOfferSkill.id)}
+                                                    >
+                                                        <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
+                                                        c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
+                                                        c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
+                                                        c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
+                                                        l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
+                                                        c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
+                                                    </svg>
+                                                </p>
+                                            </div>
                                         </div>
-                                    </div>
-                                ))}
-                        </div>
-                        <p className="mt-2">Nice to have</p>
-                        <div className='row flex mb-3 row-gap-2 column-gap-3 container'>
-                            {jobOfferSkill
-                                .filter(jobOfferSkill => jobOfferSkill.skill_type === 'optional')
-                                .map((jobOfferSkill, index) => (
-                                    <div key={index} className='col-auto p-0'>
-                                        <div className='profile-skill border border-1 text-white bg-black rounded-2 p-1 px-2 d-flex align-items-center'>
-                                            <p className='mb-0 '>{jobOfferSkill?.skill || ''}
-                                                <svg
-                                                    height="0.75rem"
-                                                    width="0.75rem"
-                                                    version="1.1"
-                                                    id="Capa_1"
-                                                    viewBox="0 0 460.775 460.775"
-                                                    className='mb-1'
-                                                    onClick={() => deleteSkill(jobOfferSkill.id)}
-                                                >
-                                                    <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
-                                                    c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
-                                                    c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
-                                                    c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
-                                                    l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
-                                                    c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
-                                                </svg>
-                                            </p>
+                                    ))}
+                            </div>
+                        </>
+                    )}
+                    {jobOfferSkill.some(skill => skill.skill_type === 'optional') && (
+                        <>
+                            <p className="mt-2">Nice to have</p>
+                            <div className='row flex mb-3 row-gap-2 column-gap-3 container'>
+                                {jobOfferSkill
+                                    .filter(jobOfferSkill => jobOfferSkill.skill_type === 'optional')
+                                    .map((jobOfferSkill, index) => (
+                                        <div key={index} className='col-auto p-0'>
+                                            <div className='profile-skill border border-1 text-white bg-black rounded-2 p-1 px-2 d-flex align-items-center'>
+                                                <p className='mb-0 '>{jobOfferSkill?.skill || ''}
+                                                    <svg
+                                                        height="0.75rem"
+                                                        width="0.75rem"
+                                                        version="1.1"
+                                                        id="Capa_1"
+                                                        viewBox="0 0 460.775 460.775"
+                                                        className='mb-1'
+                                                        onClick={() => deleteSkill(jobOfferSkill.id)}
+                                                    >
+                                                        <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
+                                                        c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
+                                                        c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
+                                                        c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55
+                                                        l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719
+                                                        c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z"/>
+                                                    </svg>
+                                                </p>
+                                            </div>
                                         </div>
-                                    </div>
-                                ))}
-                        </div>
-                    </div>
+                                    ))}
+                            </div>
+                        </>
+                    )}
+                </div>
                 </div>
         </div>
     )
